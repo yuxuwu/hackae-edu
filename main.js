0f73ae237d9874716da3a07d5b3bec37d1daf56c
Yu Xuan Wu <ywu148@binghamton.edu>
2017-11-04 23:22:35 -0400

    Got data base and registration page up

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
-const express = require("express")
-const path = require("path")
-const app = express()
+const express = require("express");
+const passport = require("passport");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+
+/*
+let db = new sqlite3.Database("./db_tables.db", function(err){
+    if(err){
+        return console.error(err.message);
+    }
+    console.log("Connected to local database")
+});
+
+//Add data to table
+db.run("INSERT INTO langs(name) VALUES(?)", ["C"], function(err){
+    if(err){
+        return console.error(err.message);
+    }
+    console.log("Added 1 row to table");
+});
+
+db.close(function(err){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log("Closed database connection")
+});
+*/
+
+const app = express();
 
 app.use(express.static("views/"))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, "views/", "main.html")));
+
+app.get('/', function(req, res){ 
+    res.sendFile(path.join(__dirname, "views/", "main.html"));
+});
+
+app.get('/org_register', function(req, res){ 
+    res.sendFile(path.join(__dirname, "views/", "org_register.html"));
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
